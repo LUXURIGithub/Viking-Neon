11e4b55494ff07b78581493ebc6fa1d813b6f1d9
LUXURIGithub <132486060+LUXURIGithub@users.noreply.github.com>
2023-07-06 21:06:07 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+let bgImage
+
+function preload() {
+  bgImage = loadImage('https://media.istockphoto.com/id/1171564349/video/retro-land.jpg?s=640x640&k=20&c=UytoK5VuR3nXezFTIryRH6D4mHUE846zUBZhlIJA5cw=')
+  
+  char1 = loadImage('https://o.remove.bg/downloads/aa7e7457-a2a3-4ad1-bec3-9070382a61fd/Character-removebg-preview.png')
+
+  char2 = loadImage('https://o.remove.bg/downloads/5b986358-8ca5-4aa4-9200-4254801ba165/char2-removebg-preview.png')
+  
+
+  char3 = loadImage('https://o.remove.bg/downloads/bfb7f1df-71d5-4819-9c5d-8a58ec0cac50/char3-removebg-preview.png')
+
+  
+}
+
+function setup() {
+  createCanvas(800, 400);
+}
+function draw() {
+  background(bgImage);
+// x(left, right).   y (up, down)
+//               x.   y. 
+//                        size size
+  image(char1, 30, 200, 100, 150)
+  image(char2, 100, 210, 150, 150)
+  image(char3, 205, 155, 200, 210)
+  
+}
