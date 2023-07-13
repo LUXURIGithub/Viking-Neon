433acd553f2772ad5d43f76a327c0e58456d1ded
LUXURIGithub <132486060+LUXURIGithub@users.noreply.github.com>
2023-07-13 20:20:36 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,28 +1,25 @@
-let bgImage
+let bgImage; 
+let bgX = 0;
+let runGif;
 
 function preload() {
-  bgImage = loadImage('https://media.istockphoto.com/id/1171564349/video/retro-land.jpg?s=640x640&k=20&c=UytoK5VuR3nXezFTIryRH6D4mHUE846zUBZhlIJA5cw=')
-  
-  char1 = loadImage('https://static.miraheze.org/ccbmwikiwiki/6/60/Gutsman.png')
-
-  char2 = loadImage('https://static.miraheze.org/ccbmwikiwiki/f/fa/Maestro.png')
-  
-
-  char3 = loadImage('https://pbs.twimg.com/media/ESqwndQUwAAY8TQ.png')
-
-  
+  bgImage = loadImage('Sprites/Parallaxbg.png')
+  Character = createImg('Sprites/Runcyclesprite.gif');
 }
 
 function setup() {
-  createCanvas(800, 400);
+  createCanvas(1920, 1080);
 }
 function draw() {
-  background(bgImage);
-// x(left, right).   y (up, down)
-//               x.   y. 
-//                        size size
-  image(char1, 30, 200, 150, 150)
-  image(char2, 200, 250, 75, 100)
-  image(char3, 300, 215, 125, 145)
+  bgX -= 12;
+
+  for (let x = bgX; x < width; x += bgImage.width) {
+    image(bgImage, x, 0);
+  }
+
+  for (let x = 0; x < bgX; x += bgImage.width) {
+    image(bgImage, x, 0);
+  }
   
+  Character.position(0, 550);
 }
