be316b2b59e550d9b69380259f1d1b6d2bd5479a
LUXURIGithub <132486060+LUXURIGithub@users.noreply.github.com>
2023-07-13 21:58:48 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 let bgImage; 
 let bgX = 0;
+let bgpX = 0;
 let runGif;
 
 function preload() {
-  bgImage = loadImage('Sprites/Parallaxbg.png')
+  bgImage = loadImage('Sprites/Parallaxbg-removebg.png')
+  bgImageparallax = loadImage('Sprites/Parallaxbg(1).png')
   Character = createImg('Sprites/Runcyclesprite.gif');
 }
 
@@ -11,15 +13,42 @@ function setup() {
   createCanvas(1920, 1080);
 }
 function draw() {
-  bgX -= 12;
+  bgX -=  6;
+  bgpX -= 7;
 
+  clear();
+
+    for (let x = bgpX; x < width; x += bgImageparallax.width) {
+  image(bgImageparallax, x, 0);
+  }
+
+  for (let x = 0; x < bgpX; x += bgImage.width) {
+    image(bgImageparallax, x, 0);
+  }
+  
   for (let x = bgX; x < width; x += bgImage.width) {
-    image(bgImage, x, 0);
+  image(bgImage, x, 0);
   }
 
   for (let x = 0; x < bgX; x += bgImage.width) {
     image(bgImage, x, 0);
   }
+  
+  
+
+
+  
+
+  
+
+  
+  
+  
+
+  
+  
+  
+  
   
   Character.position(0, 550);
 }
